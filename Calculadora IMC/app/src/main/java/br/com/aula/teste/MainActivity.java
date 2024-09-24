fb967a74cf9d19fb740a62e02706bae93349077f
JeniferMarques42 <148556837+JeniferMarques42@users.noreply.github.com>
2024-09-24 14:34:39 -0300

    atualizações app
    Criação de novos Activies para mostrar resultado da classificação de cada Imc.

--- Calculadora IMC/app/src/main/java/br/com/aula/teste/MainActivity.java
+++ Calculadora IMC/app/src/main/java/br/com/aula/teste/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.aula.teste;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,8 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.DecimalFormat;
+import java.util.Objects;
 
-import br.com.aula.teste.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,55 +43,106 @@ public class MainActivity extends AppCompatActivity {
         TextView resultado2 = findViewById(R.id.textoResultado2);
 
         // extrai dos Objetos, recuperando a String que pompões:
-        String nome = campoNome.getText().toString();
-        String peso = campoPeso.getText().toString();
-        String altura = campoAltura.getText().toString();
+        String nome = Objects.requireNonNull(campoNome.getText()).toString();
+        String peso = Objects.requireNonNull(campoPeso.getText()).toString();
+        String altura = Objects.requireNonNull(campoAltura.getText()).toString();
 
         // VALIDAÇÃO ENTRADA ZERADA
-        if(TextUtils.isEmpty(peso) || TextUtils.isEmpty(altura)){
+        if(TextUtils.isEmpty(nome)||TextUtils.isEmpty(peso) || TextUtils.isEmpty(altura)){
             resultado1.setText("ERRO!");
             resultado2.setText("ENTRADA INVALIDA!");
             return;
-        }
+        }else {
+            // limpar erro
+            resultado1.setText("");
+            resultado2.setText("");
 
-        // Converter String para Numérico
+            // Converter String para Numérico
+            Double numPeso = Double.parseDouble(peso);
+            Double numAltura = Double.parseDouble(altura);
 
-        Double numPeso = Double.parseDouble(peso);
-        Double numAltura = Double.parseDouble(altura);
+            // Calcular IMC
+            Double numImc = numPeso/(numAltura * numAltura);
 
-        // Calcular IMC
-        Double numImc = numPeso/(numAltura * numAltura);
+            // Converter o resultado nunmImc para String
+            String imc = String.valueOf(numImc);
 
-        // Converter o resultado nunmImc para String
-        String imc = String.valueOf(numImc);
+            // Formatação para apresentação do resultado:
+            DecimalFormat df = new DecimalFormat("#.##");
+            imc = df.format(numImc);
 
-        // Formatação para apresentação do resultado:
-        DecimalFormat df = new DecimalFormat("#.##");
-        imc = df.format(numImc);
+            // Apresentando o resultado para o app já formatado e validação dos dados:
+            if(numPeso <= 0 || numPeso >= 595 || numAltura <= 0 || numAltura >= 2.5){
+                resultado1.setText("ERRO!");
+                resultado2.setText("ENTRADA INVÁLIDA!");
+                return;
+            }
 
-        // Apresentando o resultado para o app já formatado e validação dos dados:
-        if(numPeso <= 0 || numPeso >= 595 || numAltura <= 0 || numAltura >= 2.5){
-            resultado1.setText("ERRO!");
-            resultado2.setText("ENTRADA INVÁLIDA!");
-            return;
-        } else{
-            resultado1.setText(imc);
-        }
+            // limpar erro
+            resultado1.setText("");
+            resultado2.setText("");
 
-        // Verificando a classificação do peso
+            // Verificando a classificação do peso
+            if(numImc < 18.5) {
+                //Chama tela 2
+                Intent intent = new Intent(this, MainActivity2.class);
 
-        if(numImc < 18.5) {
-            resultado2.setText("Baixo Peso");
-        } else if(numImc >= 18.5 && numImc <= 24.9 ){
-            resultado2.setText("Peso Normal");
-        } else if(numImc>=25 && numImc<=29.9){
-            resultado2.setText("Sobrepeso");
-        } else if(numImc>30 && numImc<=34.9){
-            resultado2.setText("Obesidade grau 1");
-        } else if(numImc>35 && numImc<=39.9){
-            resultado2.setText("Obesidade grau 2");
-        }else if(numImc>40){
-            resultado2.setText("Obesidade extrema");
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Baixo Peso");
+
+                startActivity(intent);
+
+            } else if(numImc >= 18.5 && numImc <= 24.9 ){
+                // chama tela 3
+                Intent intent = new Intent(this, MainActivity3.class);
+
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Peso Normal");
+
+                startActivity(intent);
+            } else if(numImc>=25 && numImc<=29.9){
+                //Chama tela 4
+                Intent intent = new Intent(this, MainActivity4.class);
+
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Sobrepeso");
+
+                startActivity(intent);
+            } else if(numImc>30 && numImc<=34.9){
+                //Chamar tela 5
+                Intent intent = new Intent(this, MainActivity5.class);
+
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Obesidade grau 1");
+
+                startActivity(intent);
+
+            } else if(numImc>35 && numImc<=39.9){
+                //Chamar tela 6
+
+                Intent intent = new Intent(this, MainActivity6.class);
+
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Obesidade grau 2");
+
+                startActivity(intent);
+
+            }else if(numImc>40){
+                //Chamar tela 7
+                Intent intent = new Intent(this, MainActivity7.class);
+
+                // Definição de variável e receber o conteúdo para serem transferidos
+                intent.putExtra("resultado1", imc);
+                intent.putExtra("resultado2", "Obesidade extrema");
+
+                startActivity(intent);
+
+            }
         }
     }
 
---
+++ Calculadora IMC/app/src/main/java/br/com/aula/teste/MainActivity5.java
@@ -0,0 +1,48 @@
+package br.com.aula.teste;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    private TextView textResultado1, textResultado2;
+    private Button btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main5);
+
+        // Recupera os valores passados pela Intent
+        // Recupera os valores passados pela Intent
+        String resultado1 = getIntent().getStringExtra("resultado1");
+        String resultado2 = getIntent().getStringExtra("resultado2");
+
+        // Atualiza os TextViews com os resultados
+        textResultado1 = findViewById(R.id.textResultado1);
+        textResultado2 = findViewById(R.id.textResultado2);
+
+        textResultado1.setText(resultado1);
+        textResultado2.setText(resultado2);
+
+        // Fechar Activity5
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(view ->{
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
